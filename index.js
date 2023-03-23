68b7841906efb0c751afbce7030e3927f0aea858
RicardoUy <rvuy62@yahoo.com>
2023-03-23 10:37:35 -0500

    submit functions cont.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun( task = "roller-skate"){
+     return (`This Saturday, I want to ${task}!`)
+}
+saturdayFun();
+
+saturdayFun("bathe my dog");
+
+const mondayWork = function (plan = 'go to the office'){
+    return (`This Monday, I will ${plan}.`)
+}
+mondayWork();
+mondayWork("work from home");
+
+
+
+function wrapAdjective(wrap = "*") {
+    return function(msg = "special" ){ 
+     return `You are ${wrap}${msg}${wrap}!`;
+   };
+   
+   }
+   wrapAdjective("%")("a dedicated programmer");
